54ca01c8e0942f2b9ca77582ede69a73dc3a1e0f
Seven Eda <53v3n3d4@gmail.com>
2024-04-05 23:27:04 -0300

    chore: upgrade packages

---
+++ src/js/eslint.config.js
@@ -0,0 +1,17 @@
+import { FlatCompat } from '@eslint/eslintrc'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname
+})
+
+export default [
+
+  // mimic ESLintRC-style extends
+  ...compat.extends('.eslintrc.cjs')
+]
