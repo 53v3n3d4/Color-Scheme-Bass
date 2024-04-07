0a73bcf38711df55a3b671c2a49093aea5d4e0cf
Seven Eda <53v3n3d4@gmail.com>
2024-04-07 20:24:20 -0300

    chore: clean eslint.config.js

--- src/js/eslint.config.js
+++ src/js/eslint.config.js
@@ -1,17 +1,28 @@
+// Flat config issue need publish new version to npm
+// https://github.com/standard/eslint-config-standard/issues/351
+// import standard from 'eslint-config-standard'
+
+// export default [
+//   standard,
+//   {
+//     languageOptions: {
+//       ecmaVersion: 'latest',
+//       sourceType: 'module'
+//     }
+//   },
+//   {
+//     "files": ["**/*.test.js"],
+//     "env": {
+//       "jest": true
+//     }
+//   }
+// ]
+
 import { FlatCompat } from '@eslint/eslintrc'
-import path from 'path'
-import { fileURLToPath } from 'url'
 
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname
-})
+const compat = new FlatCompat()
 
 export default [
-
   // mimic ESLintRC-style extends
   ...compat.extends('.eslintrc.cjs')
 ]
