203fad122681573bd7d060dfea558fc89ac28e27
Seven Eda <53v3n3d4@gmail.com>
2024-04-10 21:17:49 -0300

    chore: migrate to eslint flat config

--- src/js/eslint.config.js
+++ src/js/eslint.config.js
@@ -1,28 +1,26 @@
+import { FlatCompat } from '@eslint/eslintrc'
+import globals from 'globals'
 // Flat config issue need publish new version to npm
 // https://github.com/standard/eslint-config-standard/issues/351
 // import standard from 'eslint-config-standard'
 
-// export default [
-//   standard,
-//   {
-//     languageOptions: {
-//       ecmaVersion: 'latest',
-//       sourceType: 'module'
-//     }
-//   },
-//   {
-//     "files": ["**/*.test.js"],
-//     "env": {
-//       "jest": true
-//     }
-//   }
-// ]
-
-import { FlatCompat } from '@eslint/eslintrc'
-
 const compat = new FlatCompat()
 
 export default [
-  // mimic ESLintRC-style extends
-  ...compat.extends('.eslintrc.cjs')
+  // standard,
+  ...compat.extends('eslint-config-standard'),
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    }
+  },
+  {
+    files: ['**/*.test.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest
+      }
+    }
+  }
 ]
